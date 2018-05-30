345ed0bae9c4e0a1957403208b8894755cb44596
2pow <36746225+2pow@users.noreply.github.com>
2018-05-29 20:52:40 -0500

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+
+#rsync -a --progress /home/rabin/Documents /mnt/HardDisk/Documents
+#rsync -a --progress /home/rabin/Pictures /mnt/HardDisk/Pictures
+
+#cp //media/HardDisk_Part1/Documents/ 
+
+
+
+rsync -a --progress /home/rabin/Documents/ /run/media/rabin/Linux\ Backup/Documents
+rsync -a --progress /home/rabin/Pictures/ /run/media/rabin/Linux\ Backup/Pictures
+rsync -a --progress /home/rabin/Desktop/ /run/media/rabin/Linux\ Backup/Desktop
+
+
+
+
+
+
+#rsync -a --progress /home/rabin/ /home/rabin/Desktop/Linux\ Backup/
+
+#date
+
+echo "Last Backup made on";date
